dfbd6999a57f2f2b5a322df762091dcdac035672
ashma798 <53849922+ashma798@users.noreply.github.com>
2025-03-26 09:02:58 +0530

    Update axiosConfig.js

--- frontend/src/axiosConfig/axiosConfig.js
+++ frontend/src/axiosConfig/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 const token = localStorage.getItem('@token');
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    //baseURL: 'http://localhost:5000/api',
+     baseURL: 'https://e-commerce-mern-2mad.onrender.com/api',
     timeout: 5000,
         headers: {
            'Authorization': token ? `Bearer ${JSON.parse(token)}` : '',
